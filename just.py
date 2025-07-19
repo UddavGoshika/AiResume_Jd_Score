091b9d96cc26083412477ecfd81dfd1c5363b020
Goshika Uddav <138785489+UddavGoshika@users.noreply.github.com>
2025-07-19 08:19:17 +0530

    Delete just.py

--- just.py
+++
@@ -1,9 +0,0 @@
-import cohere
-
-co = cohere.ClientV2("qr4pr7IfGpLfFzm10eyUTH7UrYClOh4snS8NhBgF")
-response = co.chat(
-    model="command-a-03-2025", 
-    messages=[{"role": "user", "content": "what is the blackhole ?"}]
-)
-anser = response.message.content[0].text
-print(anser)
