474b6c3c8da9bb25c8763fea155e59aef8a6b4d8
Goshika Uddav <138785489+UddavGoshika@users.noreply.github.com>
2025-07-19 10:27:40 +0530

    Update Index.py

--- Index.py
+++ Index.py
@@ -59,15 +59,15 @@ with col1:
     resume_file = st.file_uploader("📄 Upload Resume (PDF or DOCX)", type=["pdf", "docx"], key="resume_upload")
 with col2:
     jd_text = st.text_area("📃 Job Description (Paste text here)", "")
-import re
-match_score_section = result.split("Missing/Weak Keywords:")[0].strip()
+# import re
+# match_score_section = result.split("Missing/Weak Keywords:")[0].strip()
 
-match_score = re.search(r"(\d+)/100", match_score_section)
+# match_score = re.search(r"(\d+)/100", match_score_section)
 
 
-if match_score:
-    score = int(match_score.group(1))
-    st.progress(score / 100)
+# if match_score:
+#     score = int(match_score.group(1))
+#     st.progress(score / 100)
 
 
 if resume_file and jd_text.strip():
